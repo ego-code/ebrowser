a9ece6b39c925a4f9d34ee8544a96f8e090a313e
ego-code <72394892+ego-code@users.noreply.github.com>
2021-06-13 07:11:00 +0800

    Add files via upload

---
+++ code/init.lua
@@ -0,0 +1,13 @@
+appcode="1"
+template="tool"
+appname="轻量浏览器"
+packagename="com.browser.ego"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appver="1.1"
